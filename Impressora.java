f327b0e40be701de693ae5af375ea9577b5d756f
Bruna Barbosa <brunabarbosa_snts@hotmail.com>
2022-09-22 17:59:34 -0300

    exercico 1.2

--- Impressora.java
+++ Impressora.java
@@ -1,7 +1,12 @@
 public class Impressora{
-
-    public void exibir ( float a){
-        System.out.print(a);
+//1.1
+    public void exibir ( float b){
+        System.out.print(b);
     }
 
+//1.2
+
+public float exibir (float a, float c){
+        return a + c;
+}
 }
--- TesteImpressora.java
+++ TesteImpressora.java
@@ -1,6 +1,10 @@
 public class TesteImpressora{
-
     public static void main (String [] args ){
+// 1.2
+        Impressora calc = new Impressora();
+        float a = 5.0F, c=2.7F;
+        float x = calc.exibir (a , c);
+        System.out.println(x);
         
     }
 }

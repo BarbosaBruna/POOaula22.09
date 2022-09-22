cbfa72f40152b97d865c31acd8461c228cb61ebc
Bruna Barbosa <brunabarbosa_snts@hotmail.com>
2022-09-22 17:26:14 -0300

    initial commit

---
+++ Impressora.java
@@ -0,0 +1,7 @@
+public class Impressora{
+
+    public float soma ( int a, int b){
+        return a + b;
+    }
+
+}
---
+++ TesteImpressora.java
@@ -0,0 +1,6 @@
+public class TesteImpressora{
+
+    public static void main (String [] args ){
+        
+    }
+}
